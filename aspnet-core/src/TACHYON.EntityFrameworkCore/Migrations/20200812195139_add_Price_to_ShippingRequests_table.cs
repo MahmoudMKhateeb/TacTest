a69491b17880fc74c94354202454a6ed8a2fe243
Islam AlKhateeb <ialkhateeb@trustangle.com>
2020-08-13 00:54:22 +0300

    add Price to ShippingRequests

---
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20200812195139_add_Price_to_ShippingRequests_table.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TACHYON.Migrations
+{
+    public partial class add_Price_to_ShippingRequests_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "ShippingRequests",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "ShippingRequests");
+        }
+    }
+}
--- aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20200812195333_make_Price_in_ShippingRequests_nullable.Designer.cs
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20200812195333_make_Price_in_ShippingRequests_nullable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TACHYON.EntityFrameworkCore;
 
 namespace TACHYON.Migrations
 {
     [DbContext(typeof(TACHYONDbContext))]
-    partial class TACHYONDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200812195333_make_Price_in_ShippingRequests_nullable")]
+    partial class make_Price_in_ShippingRequests_nullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
