7f6d4ef46052ddca36c1477535da98730038db2d
Islam AlKhateeb <ialkhateeb@TRUSTANGLE.COM>
2020-09-29 02:40:39 +0300

    required documents from tenant

--- angular/src/app/admin/required-document-files/required-document-files.component.ts
+++ angular/src/app/admin/required-document-files/required-document-files.component.ts
@@ -2,15 +2,21 @@ import { Component, Injector, OnInit } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { FileItem, FileUploader, FileUploaderOptions } from '@node_modules/ng2-file-upload';
 import { AppConsts } from '@shared/AppConsts';
-import { IAjaxResponse } from '@node_modules/abp-ng2-module';
-import { CreateOrEditDocumentFileDto, UpdateDocumentFileInput } from '@shared/service-proxies/service-proxies';
+import { IAjaxResponse, TokenService } from '@node_modules/abp-ng2-module';
+import { CreateOrEditDocumentFileDto, DocumentFilesServiceProxy, UpdateDocumentFileInput } from '@shared/service-proxies/service-proxies';
 import { finalize } from '@node_modules/rxjs/operators';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
+import * as moment from '@node_modules/moment';
 
 @Component({
   selector: 'app-required-document-files',
   templateUrl: './required-document-files.component.html',
+  animations: [appModuleAnimation()],
 })
 export class RequiredDocumentFilesComponent extends AppComponentBase implements OnInit {
+  active = false;
+  saving = false;
+
   /**
    * required documents fileUploader
    */
@@ -30,74 +36,81 @@ export class RequiredDocumentFilesComponent extends AppComponentBase implements 
    */
   private _DocsUploaderOptions: FileUploaderOptions = {};
 
-  constructor(injector: Injector) {
+  createOrEditDocumentFileDtos: CreateOrEditDocumentFileDto[];
+
+  constructor(injector: Injector, private _documentFilesServiceProxy: DocumentFilesServiceProxy, private _tokenService: TokenService) {
     super(injector);
+    this._documentFilesServiceProxy.getTenantRequiredDocumentFileList().subscribe((result) => {
+      result.forEach((x) => (x.expirationDate = null));
+      this.createOrEditDocumentFileDtos = result;
+      this.active = true;
+    });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initDocsUploader();
+  }
 
   /**
    * initialize required documents fileUploader
    */
-  // initDocsUploader(): void {
-  //   this.DocsUploader = new FileUploader({ url: AppConsts.remoteServiceBaseUrl + '/Helper/UploadDocumentFile' });
-  //   this._DocsUploaderOptions.autoUpload = false;
-  //   this._DocsUploaderOptions.authToken = 'Bearer ' + this._tokenService.getToken();
-  //   this._DocsUploaderOptions.removeAfterUpload = true;
-  //
-  //   this.DocsUploader.onAfterAddingFile = (file) => {
-  //     file.withCredentials = false;
-  //   };
-  //
-  //   this.DocsUploader.onBuildItemForm = (fileItem: FileItem, form: any) => {
-  //     form.append('FileType', fileItem.file.type);
-  //     form.append('FileName', fileItem.file.name);
-  //     form.append('FileToken', this.guid());
-  //   };
-  //
-  //   this.DocsUploader.onSuccessItem = (item, response, status) => {
-  //     const resp = <IAjaxResponse>JSON.parse(response);
-  //
-  //     if (resp.success) {
-  //       //attach each fileToken to his CreateOrEditDocumentFileDto
-  //       this.truck.createOrEditDocumentFileDtos.find(
-  //         (x) => x.name === item.file.name && x.extn === item.file.type
-  //       ).updateDocumentFileInput = new UpdateDocumentFileInput({ fileToken: resp.result.fileToken });
-  //     } else {
-  //       this.message.error(resp.error.message);
-  //     }
-  //   };
-  //
-  //   this.DocsUploader.onErrorItem = (item, response, status) => {
-  //     const resp = <IAjaxResponse>JSON.parse(response);
-  //     console.log(resp);
-  //   };
-  //
-  //   this.DocsUploader.onCompleteAll = () => {
-  //     // create truck req.
-  //     this._trucksServiceProxy
-  //       .createOrEdit(this.truck)
-  //       .pipe(
-  //         finalize(() => {
-  //           this.saving = false;
-  //         })
-  //       )
-  //       .subscribe(() => {
-  //         this.saving = false;
-  //         this.notify.info(this.l('SavedSuccessfully'));
-  //         this.close();
-  //         this.modalSave.emit(null);
-  //       });
-  //   };
-  //
-  //   //for progressBar
-  //   this.DocsUploader.onProgressItem = (fileItem: FileItem, progress: any) => {
-  //     this.docProgress = progress;
-  //     this.docProgressFileName = fileItem.file.name;
-  //   };
-  //
-  //   this.DocsUploader.setOptions(this._DocsUploaderOptions);
-  // }
+  initDocsUploader(): void {
+    this.DocsUploader = new FileUploader({ url: AppConsts.remoteServiceBaseUrl + '/Helper/UploadDocumentFile' });
+    this._DocsUploaderOptions.autoUpload = false;
+    this._DocsUploaderOptions.authToken = 'Bearer ' + this._tokenService.getToken();
+    this._DocsUploaderOptions.removeAfterUpload = true;
+
+    this.DocsUploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+
+    this.DocsUploader.onBuildItemForm = (fileItem: FileItem, form: any) => {
+      form.append('FileType', fileItem.file.type);
+      form.append('FileName', fileItem.file.name);
+      form.append('FileToken', this.guid());
+    };
+
+    this.DocsUploader.onSuccessItem = (item, response, status) => {
+      const resp = <IAjaxResponse>JSON.parse(response);
+
+      if (resp.success) {
+        //attach each fileToken to his CreateOrEditDocumentFileDto
+        this.createOrEditDocumentFileDtos.find(
+          (x) => x.name === item.file.name && x.extn === item.file.type
+        ).updateDocumentFileInput = new UpdateDocumentFileInput({ fileToken: resp.result.fileToken });
+      } else {
+        this.message.error(resp.error.message);
+      }
+    };
+
+    this.DocsUploader.onErrorItem = (item, response, status) => {
+      const resp = <IAjaxResponse>JSON.parse(response);
+      console.log(resp);
+    };
+
+    this.DocsUploader.onCompleteAll = () => {
+      // create truck req.
+      this._documentFilesServiceProxy
+        .addTenantRequiredDocuments('', this.createOrEditDocumentFileDtos)
+        .pipe(
+          finalize(() => {
+            this.saving = false;
+          })
+        )
+        .subscribe(() => {
+          this.saving = false;
+          this.notify.info(this.l('SavedSuccessfully'));
+        });
+    };
+
+    //for progressBar
+    this.DocsUploader.onProgressItem = (fileItem: FileItem, progress: any) => {
+      this.docProgress = progress;
+      this.docProgressFileName = fileItem.file.name;
+    };
+
+    this.DocsUploader.setOptions(this._DocsUploaderOptions);
+  }
 
   DocFileChangeEvent(event: any, item: CreateOrEditDocumentFileDto): void {
     if (event.target.files[0].size > 5242880) {
@@ -119,5 +132,10 @@ export class RequiredDocumentFilesComponent extends AppComponentBase implements 
     }
 
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
+
+  save(): void {
+    this.saving = true;
+    this.DocsUploader.uploadAll();
   }
 }
