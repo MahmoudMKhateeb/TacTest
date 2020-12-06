50080d94a20ae06496a025a6ce5de7e4b7a55d59
Islam AlKhateeb <ialkhateeb@TRUSTANGLE.COM>
2020-12-06 19:28:27 +0200

    Add import driver from excel feature

---
+++ aspnet-core/src/TACHYON.Application/Drivers/importing/Dto/ImportDriverDto.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using TACHYON.Documents.DocumentFiles.Dtos;
+
+namespace TACHYON.Drivers.importing.Dto
+{
+    public class ImportDriverDto
+    {
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string UserName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Can be set when reading data from excel or when importing user
+        /// </summary>
+        public string Exception { get; set; }
+
+        public bool CanBeImported()
+        {
+            return string.IsNullOrEmpty(Exception);
+        }
+
+        public bool IsDriver { get; set; }
+
+        public List<CreateOrEditDocumentFileDto> CreateOrEditDocumentFileDtos { get; set; }
+
+
+    }
+}
---
+++ aspnet-core/src/TACHYON.Application/Drivers/importing/IDriverListExcelDataReader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Abp.Dependency;
+using TACHYON.Authorization.Users.Importing.Dto;
+using TACHYON.Drivers.importing.Dto;
+
+namespace TACHYON.Drivers.importing
+{
+    public interface IDriverListExcelDataReader : ITransientDependency
+    {
+        List<ImportDriverDto> GetDriversFromExcel(byte[] fileBytes);
+    }
+}
