9a941d2f302308ab055823bf5864df5bd4a7532d
Mahmoud Kh <malkhateeb@trustangle.com>
2021-03-14 14:06:39 +0200

    New CreateOrEdit Facility Modal
    Facility Modal Map Integration and Ui Improvments

--- angular/src/app/main/addressBook/facilities/create-or-edit-facility-modal.component.ts
+++ angular/src/app/main/addressBook/facilities/create-or-edit-facility-modal.component.ts
@@ -1,7 +1,13 @@
-﻿import { Component, ViewChild, Injector, Output, EventEmitter, NgZone, ElementRef, OnInit } from '@angular/core';
+﻿import { Component, ViewChild, Injector, Output, EventEmitter, NgZone, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { finalize } from 'rxjs/operators';
-import { FacilitiesServiceProxy, CreateOrEditFacilityDto, FacilityCityLookupTableDto } from '@shared/service-proxies/service-proxies';
+import {
+  FacilitiesServiceProxy,
+  CreateOrEditFacilityDto,
+  FacilityCityLookupTableDto,
+  CountiesServiceProxy,
+  TenantRegistrationServiceProxy,
+} from '@shared/service-proxies/service-proxies';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { MapsAPILoader } from '@node_modules/@agm/core';
 
@@ -12,29 +18,25 @@ import { MapsAPILoader } from '@node_modules/@agm/core';
 export class CreateOrEditFacilityModalComponent extends AppComponentBase implements OnInit {
   @ViewChild('createOrEditFacilityModal', { static: true }) modal: ModalDirective;
   @ViewChild('search') public searchElementRef: ElementRef;
-
+  @ViewChild('secountInput') public secountInput: ElementRef;
   @Output() modalSave: EventEmitter<any> = new EventEmitter<any>();
-
   zoom = 14;
   active = false;
   saving = false;
   facility: CreateOrEditFacilityDto = new CreateOrEditFacilityDto();
+  countries: any;
+  cities: any;
   private geoCoder;
-
+  allCities: FacilityCityLookupTableDto[];
+  countriesLoading: boolean;
+  citiesLoading: boolean;
+  selectedCountryId: number;
   selectedCountryCode = 'SA';
-  countyDisplayName = '';
-  cityDisplayName = '';
-
-  Address: string;
-  State: string;
-  Postal: string;
-  City: string;
-  Country: string;
-  allCitys: FacilityCityLookupTableDto[];
 
   constructor(
     injector: Injector,
     private _facilitiesServiceProxy: FacilitiesServiceProxy,
+    private _countriesServiceProxy: TenantRegistrationServiceProxy,
     private ngZone: NgZone,
     private mapsAPILoader: MapsAPILoader
   ) {
@@ -42,6 +44,7 @@ export class CreateOrEditFacilityModalComponent extends AppComponentBase impleme
   }
   ngOnInit() {
     this.loadMapApi();
+    this.loadAllCountries();
     this.facility.latitude = 24.67911662122269;
     this.facility.longitude = 46.6355543345471;
   }
@@ -50,30 +53,26 @@ export class CreateOrEditFacilityModalComponent extends AppComponentBase impleme
     if (!facilityId) {
       this.facility = new CreateOrEditFacilityDto();
       this.facility.id = facilityId;
-      this.countyDisplayName = '';
-      this.cityDisplayName = '';
 
       this.active = true;
       this.modal.show();
     } else {
       this._facilitiesServiceProxy.getFacilityForEdit(facilityId).subscribe((result) => {
         this.facility = result.facility;
-
-        this.cityDisplayName = result.cityDisplayName;
-
         this.active = true;
         this.modal.show();
       });
     }
     this._facilitiesServiceProxy.getAllCityForTableDropdown().subscribe((result) => {
-      this.allCitys = result;
+      this.allCities = result;
     });
-    console.log('From Facility Modal');
   }
 
+  /**
+   * CreateOrUpdate Facility
+   */
   save(): void {
     this.saving = true;
-
     this._facilitiesServiceProxy
       .createOrEdit(this.facility)
       .pipe(
@@ -92,11 +91,14 @@ export class CreateOrEditFacilityModalComponent extends AppComponentBase impleme
     this.active = false;
     this.modal.hide();
   }
-  //Facility Map Functions
+
+  /**
+   * loads Google Map API
+   * Setting Up AutoComplete For Facility Address Lookup
+   */
   loadMapApi() {
     this.mapsAPILoader.load().then(() => {
       this.geoCoder = new google.maps.Geocoder();
-
       let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
         componentRestrictions: {
           country: this.selectedCountryCode,
@@ -104,15 +106,10 @@ export class CreateOrEditFacilityModalComponent extends AppComponentBase impleme
       });
       autocomplete.addListener('place_changed', () => {
         this.ngZone.run(() => {
-          //get the place result
           let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
-          //verify result
           if (place.geometry === undefined || place.geometry === null) {
             return;
           }
-
-          //set latitude, longitude and zoom
           this.facility.latitude = place.geometry.location.lat();
           this.facility.longitude = place.geometry.location.lng();
           this.getAddress(place.geometry.location.lat(), place.geometry.location.lng());
@@ -121,54 +118,63 @@ export class CreateOrEditFacilityModalComponent extends AppComponentBase impleme
       });
     });
   }
+
+  /**
+   * Converts The Cordinates into Real Address and Assighn it to facility.address
+   * @param latitude
+   * @param longitude
+   */
   getAddress(latitude, longitude) {
     this.geoCoder.geocode({ location: { lat: latitude, lng: longitude } }, (results, status) => {
       if (status === 'OK') {
         if (results[0]) {
           this.zoom = 14;
-          let Spleted = results[0].formatted_address.split(',');
-          console.log('Address Should Be Changed By Now');
-          this.addressFormater(Spleted);
+          this.facility.address = results[0].formatted_address;
         } else {
           window.alert('No results found');
         }
       } else {
         window.alert('Geocoder failed due to: ' + status);
-      }
+      } //
     });
   }
-  addressFormater(Spleted) {
-    switch (Spleted.length) {
-      case 4:
-        this.Address = Spleted[0];
-        this.State = Spleted[1];
-        this.City = Spleted[2];
-        this.Country = Spleted[3];
-        break;
-      case 5:
-        this.Address = Spleted[0] + ' ' + Spleted[1];
-        this.State = Spleted[2];
-        this.City = Spleted[3];
-        this.Country = Spleted[4];
-        break;
-      default:
-        this.Address = undefined;
-        this.State = undefined;
-        this.City = undefined;
-        this.Country = undefined;
-        break;
-    }
-  }
+
+  /**
+   * Getting The Map Marker Cordinates from The Click
+   * @param $event: MouseEvent
+   */
   mapClicked($event: MouseEvent) {
-    this.Address = undefined;
-    this.City = undefined;
-    this.State = undefined;
-    this.Country = undefined;
     // @ts-ignore
     this.facility.latitude = $event.coords.lat;
     // @ts-ignore
     this.facility.longitude = $event.coords.lng;
     // @ts-ignore
     this.getAddress($event.coords.lat, $event.coords.lng);
+  }
+
+  /**
+   * Loads All Countires for Facilities CRUD
+   */
+  loadAllCountries() {
+    this.countriesLoading = true;
+    this._countriesServiceProxy.getAllCountryForTableDropdown().subscribe((res) => {
+      this.countries = res;
+      this.countriesLoading = false;
+    });
+  }
+
+  /**
+   * Loads All Cities After Selecting The Country
+   * @Input selectedCountryId
+   */
+
+  loadCitiesByCountryId() {
+    this.selectedCountryCode = this.countries.find((x) => x.id == this.selectedCountryId).code;
+    this.loadMapApi();
+    this.citiesLoading = true;
+    this._countriesServiceProxy.getAllCitiesForTableDropdown(this.selectedCountryId).subscribe((res) => {
+      this.allCities = res;
+      this.citiesLoading = false;
+    });
   }
 }
