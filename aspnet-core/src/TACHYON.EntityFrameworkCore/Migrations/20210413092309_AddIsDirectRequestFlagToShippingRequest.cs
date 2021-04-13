21ca2744980093819abf65e5507ffd94952a9fa6
tasneem moh <toudah@trustangle.com>
2021-04-13 12:26:12 +0300

    database add migration

---
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20210413092309_AddIsDirectRequestFlagToShippingRequest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TACHYON.Migrations
+{
+    public partial class AddIsDirectRequestFlagToShippingRequest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ShippingRequestCarrierDirectPricingId",
+                table: "TachyonPriceOffers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDirectRequest",
+                table: "ShippingRequests",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TachyonPriceOffers_ShippingRequestCarrierDirectPricingId",
+                table: "TachyonPriceOffers",
+                column: "ShippingRequestCarrierDirectPricingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TachyonPriceOffers_ShippingRequestsCarrierDirectPricing_ShippingRequestCarrierDirectPricingId",
+                table: "TachyonPriceOffers",
+                column: "ShippingRequestCarrierDirectPricingId",
+                principalTable: "ShippingRequestsCarrierDirectPricing",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TachyonPriceOffers_ShippingRequestsCarrierDirectPricing_ShippingRequestCarrierDirectPricingId",
+                table: "TachyonPriceOffers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TachyonPriceOffers_ShippingRequestCarrierDirectPricingId",
+                table: "TachyonPriceOffers");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingRequestCarrierDirectPricingId",
+                table: "TachyonPriceOffers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDirectRequest",
+                table: "ShippingRequests");
+        }
+    }
+}
