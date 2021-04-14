b64be9aba3fd3e6c4278c424efb40106595906a7
Abdullah Shaqalaih <abdullah@trustangle.com>
2021-04-14 13:13:41 +0300

    TAC-577

---
+++ aspnet-core/src/TACHYON.Application/Exporting/ExcelExporterManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using TACHYON.DataExporting.Excel.NPOI;
+using TACHYON.Dto;
+using TACHYON.Storage;
+using System.Linq;
+using Abp.Dependency;
+
+namespace TACHYON.Exporting
+{
+    public  class ExcelExporterManager<TSource>: NpoiExcelExporterBase, IExcelExporterManager<TSource>
+    {
+        public ExcelExporterManager(ITempFileCacheManager tempFileCacheManager) :base(tempFileCacheManager){}
+        public FileDto ExportToFile(List<TSource> Sources,string SheetName, string[] headerTexts, Func<TSource, object>[] propertySelectors)
+        {
+            return CreateExcelPackage(
+                $"{SheetName}",
+                excelPackage =>
+                {
+                    var sheet = excelPackage.CreateSheet(L(SheetName));
+                    AddHeader(sheet, headerTexts.Select(x=>L(x)).ToArray());
+                    AddObjects(sheet, 1, Sources, propertySelectors);
+                });
+        }
+    }
+}
