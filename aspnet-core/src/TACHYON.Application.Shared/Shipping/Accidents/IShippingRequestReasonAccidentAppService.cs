5930657546a69d6f6843c8e297c52d049461e646
Abdullah Shaqalaih <abdullah@trustangle.com>
2021-04-27 14:48:47 +0300

    TAC-729,TAC-723

--- aspnet-core/src/TACHYON.Application.Shared/Shipping/Accidents/IShippingRequestReasonAccidentAppService.cs
+++ aspnet-core/src/TACHYON.Application.Shared/Shipping/Accidents/IShippingRequestReasonAccidentAppService.cs
@@ -1,17 +1,20 @@
 ﻿using Abp.Application.Services;
 using Abp.Application.Services.Dto;
 using System.Threading.Tasks;
+using TACHYON.Common.Dto;
+using TACHYON.Dto;
 using TACHYON.Shipping.Accidents.Dto;
 
 namespace TACHYON.Shipping.Accidents
 {
     public  interface IShippingRequestReasonAccidentAppService: IApplicationService
     {
-        ListResultDto<ShippingRequestReasonAccidentListDto> GetAll(GetAllForShippingRequestReasonAccidentFilterInput Input);
+        ListResultDto<ShippingRequestReasonAccidentListDto> GetAll(FilterInput Input);
 
         Task CreateOrEdit(CreateOrEditShippingRequestReasonAccidentDto input);
         Task<CreateOrEditShippingRequestReasonAccidentDto> GetForEdit(EntityDto input);
 
+        FileDto Exports(FilterInput Input);
 
         Task Delete(EntityDto input);
     }
