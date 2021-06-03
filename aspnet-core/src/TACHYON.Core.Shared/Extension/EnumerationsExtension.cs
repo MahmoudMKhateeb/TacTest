e8fe321b2fb97d1d3216dcbfaf087ada579bea04
Abdullah Shaqalaih <abdullah@trustangle.com>
2021-06-03 16:05:49 +0300

    TAC-1051

--- aspnet-core/src/TACHYON.Core.Shared/Extension/EnumerationsExtension.cs
+++ aspnet-core/src/TACHYON.Core.Shared/Extension/EnumerationsExtension.cs
@@ -8,14 +8,22 @@ namespace TACHYON
     {
         public static string GetEnumDescription(this Enum value)
         {
-            // Get the Description attribute value for the enum value
-            FieldInfo fi = value.GetType().GetField(value.ToString());
-            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            try
+            {
+                // Get the Description attribute value for the enum value
+                FieldInfo fi = value.GetType().GetField(value.ToString());
+                DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
 
-            if (attributes.Length > 0)
-                return attributes[0].Description;
-            else
-                return value.ToString();
+                if (attributes.Length > 0)
+                    return attributes[0].Description;
+                else
+                    return value.ToString();
+            }
+            catch 
+            {
+                return "";
+            }
+
         }
     }
 }
