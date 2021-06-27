d414e5198e088697b4d40862cd00fac5b9fc6a90
tasneem <tasneemohd89@gmail.com>
2021-06-27 20:26:32 +0300

    other trip accedent reason

--- aspnet-core/src/TACHYON.Application.Shared/Shipping/Trips/Accidents/Dto/ShippingRequestTripAccidentListDto.cs
+++ aspnet-core/src/TACHYON.Application.Shared/Shipping/Trips/Accidents/Dto/ShippingRequestTripAccidentListDto.cs
@@ -9,6 +9,7 @@ namespace TACHYON.Shipping.Trips.Accidents.Dto
         public string PickingType { get; set; }
         public string Address { get; set; }
         public string Reason { get; set; }
+        public string OtherReason { get; set; }
         public string Description { get; set; }
         public bool IsResolve { get; set; }
         public Guid? DocumentId { get; set; }
