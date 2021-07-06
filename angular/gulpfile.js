dcf04982268bc9687304dea0452a8a2c0f724edb
Mahmoud Alkhateeb <malkhateeb@trustangle.com>
2021-07-06 19:46:48 +0000

    Merge Branch 'feature/TAC-1349' into develop

--- angular/gulpfile.js
+++ angular/gulpfile.js
@@ -1,6 +1,6 @@
-﻿var gulp = require("gulp");
+﻿var gulp = require('gulp');
 var path = require('path');
-var merge = require("merge-stream");
+var merge = require('merge-stream');
 var globby = require('globby');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
@@ -14,125 +14,117 @@ var styleEntries = {};
 var scriptEntries = {};
 
 function processInputDefinition(input) {
-    var result = [];
-    for (var i = 0; i < input.length; i++) {
-        var url = input[i];
-        if (url.startsWith('!')) {
-            result.push('!' + path.resolve(__dirname, url.substring(1)));
-        } else {
-            result.push(path.resolve(__dirname, url));
-        }
+  var result = [];
+  for (var i = 0; i < input.length; i++) {
+    var url = input[i];
+    var longPath = '';
+    if (url.startsWith('!')) {
+      longPath = '!' + path.resolve(__dirname, url.substring(1));
+    } else {
+      longPath = path.resolve(__dirname, url);
     }
 
-    return result;
+    longPath = longPath.replace(/\\/g, '/');
+    result.push(longPath);
+  }
+
+  return result;
 }
 
 function fillScriptBundles() {
-    // User defined bundles
-    for (var k = 0; k < bundleConfig.scripts.length; k++) {
-        var scriptBundle = bundleConfig.scripts[k];
-        scriptEntries[scriptBundle.output] = globby.sync(processInputDefinition(scriptBundle.input), { noext: true });
-    }
+  // User defined bundles
+  for (var k = 0; k < bundleConfig.scripts.length; k++) {
+    var scriptBundle = bundleConfig.scripts[k];
+    scriptEntries[scriptBundle.output] = globby.sync(processInputDefinition(scriptBundle.input), { noext: true });
+  }
 }
 
 function fillStyleBundles() {
-    // User defined styles
-    for (var k = 0; k < bundleConfig.styles.length; k++) {
-        var styleBundle = bundleConfig.styles[k];
-        styleEntries[styleBundle.output] = globby.sync(processInputDefinition(styleBundle.input), { noext: true });
-    }
+  // User defined styles
+  for (var k = 0; k < bundleConfig.styles.length; k++) {
+    var styleBundle = bundleConfig.styles[k];
+    styleEntries[styleBundle.output] = globby.sync(processInputDefinition(styleBundle.input), { noext: true });
+  }
 }
 
 function getFileNameFromPath(path) {
-    return path.substring(path.lastIndexOf('/') + 1);
+  return path.substring(path.lastIndexOf('/') + 1);
 }
 
 function getPathWithoutFileNameFromPath(path) {
-    return path.substring(0, path.lastIndexOf('/'));
+  return path.substring(0, path.lastIndexOf('/'));
 }
 
 function createScriptBundles() {
-    var tasks = [];
-    for (var script in scriptEntries) {
-        tasks.push(
-            createScriptBundle(script)
-        );
-    }
+  var tasks = [];
+  for (var script in scriptEntries) {
+    tasks.push(createScriptBundle(script));
+  }
 
-    return tasks;
+  return tasks;
 }
 
 function createScriptBundle(script) {
-    var bundleName = getFileNameFromPath(script);
-    var bundlePath = getPathWithoutFileNameFromPath(script);
+  var bundleName = getFileNameFromPath(script);
+  var bundlePath = getPathWithoutFileNameFromPath(script);
 
-    var stream = gulp.src(scriptEntries[script]);
+  var stream = gulp.src(scriptEntries[script]);
 
-    if (production) {
-        stream = stream
-            .pipe(uglify());
-    }
+  if (production) {
+    stream = stream.pipe(uglify());
+  }
 
-    return stream.pipe(concat(bundleName))
-        .pipe(gulp.dest(bundlePath));
+  return stream.pipe(concat(bundleName)).pipe(gulp.dest(bundlePath));
 }
 
 function createStyleBundles() {
-    var tasks = [];
-    for (var style in styleEntries) {
-        tasks.push(
-            createStyleBundle(style)
-        );
-    }
+  var tasks = [];
+  for (var style in styleEntries) {
+    tasks.push(createStyleBundle(style));
+  }
 
-    return tasks;
+  return tasks;
 }
 
 function createStyleBundle(style) {
+  var bundleName = getFileNameFromPath(style);
+  var bundlePath = getPathWithoutFileNameFromPath(style);
 
-    var bundleName = getFileNameFromPath(style);
-    var bundlePath = getPathWithoutFileNameFromPath(style);    
+  var stream = gulp.src(styleEntries[style]).pipe(less({ math: 'parens-division' }));
 
-    var stream = gulp.src(styleEntries[style])
-        .pipe(less({ math: 'parens-division' }));
+  if (production) {
+    stream = stream.pipe(cleanCss());
+  }
 
-    if (production) {
-        stream = stream.pipe(cleanCss());
-    }
-
-    return stream
-        .pipe(concat(bundleName))
-        .pipe(gulp.dest(bundlePath));
+  return stream.pipe(concat(bundleName)).pipe(gulp.dest(bundlePath));
 }
 
 function build(done) {
+  production = true;
 
-    production = true;
+  fillScriptBundles();
+  fillStyleBundles();
 
-    fillScriptBundles();
-    fillStyleBundles();
+  var scriptTasks = createScriptBundles();
+  var styleTasks = createStyleBundles();
 
-    var scriptTasks = createScriptBundles();
-    var styleTasks = createStyleBundles();
+  var stream = merge(scriptTasks.concat(styleTasks));
 
-    var stream = merge(scriptTasks.concat(styleTasks));
-
-    return !stream.isEmpty() ? stream : done();
+  return !stream.isEmpty() ? stream : done();
 }
 
 function buildDev(done) {
+  fillScriptBundles();
+  fillStyleBundles();
 
-    fillScriptBundles();
-    fillStyleBundles();
+  var scriptTasks = createScriptBundles();
+  var styleTasks = createStyleBundles();
 
-    var scriptTasks = createScriptBundles();
-    var styleTasks = createStyleBundles();
+  console.log('Dynamic bundles are being created.');
 
-    console.log("Dynamic bundles are being created.");
+  var stream = merge(scriptTasks.concat(styleTasks));
 
-    var stream = merge(scriptTasks.concat(styleTasks));
-
-    return !stream.isEmpty() ? stream : done();
+  return !stream.isEmpty() ? stream : done();
 }
 
 exports.build = build;
--- angular/src/app/main/priceoffer/price-offer-model-component.ts
+++ angular/src/app/main/priceoffer/price-offer-model-component.ts
@@ -18,7 +18,7 @@ import { finalize } from 'rxjs/operators';
 
 @Component({
   templateUrl: './price-offer-model-component.html',
-  styleUrls: ['./model.scss'],
+  // styleUrls: ['/assets/custom/css/model.scss'],
   selector: 'price-offer-model',
   animations: [appModuleAnimation()],
   providers: [EnumToArrayPipe],
