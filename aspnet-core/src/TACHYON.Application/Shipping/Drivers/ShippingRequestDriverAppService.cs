1c94393fe6a1f1460d3f139fe29743e6e98f6256
tasneem <tasneemohd89@gmail.com>
2021-07-25 12:44:20 +0300

    fix multi drops trips status ..

--- aspnet-core/src/TACHYON.Application/Shipping/Drivers/ShippingRequestDriverAppService.cs
+++ aspnet-core/src/TACHYON.Application/Shipping/Drivers/ShippingRequestDriverAppService.cs
@@ -309,7 +309,7 @@ namespace TACHYON.Shipping.Drivers
 
                 if (CurrentPoint.PickingType == PickingType.Pickup && trip.RoutePointStatus != RoutePointStatus.FinishLoading) throw new UserFriendlyException(L("TheTripIsNotFound"));
 
-                if (trip.RoutePointStatus == RoutePointStatus.FinishLoading)
+                if (trip.RoutePointStatus == RoutePointStatus.FinishLoading || trip.RoutePointStatus == RoutePointStatus.DeliveryConfirmation)
                 {
                     CurrentPoint.IsActive = false;
                     CurrentPoint.IsComplete = true;
