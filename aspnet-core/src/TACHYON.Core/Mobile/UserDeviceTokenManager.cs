099142f399f54694a401dd4cfcc996f3841bf4d6
islam <ialkhateeb@trustangle.com>
2021-08-12 10:36:52 +0300

    Remove mobile tokens after logOut

--- aspnet-core/src/TACHYON.Core/Mobile/UserDeviceTokenManager.cs
+++ aspnet-core/src/TACHYON.Core/Mobile/UserDeviceTokenManager.cs
@@ -1,5 +1,9 @@
 ﻿using Abp.Domain.Repositories;
+using Abp.Extensions;
+using JetBrains.Annotations;
+using System;
 using System.Threading.Tasks;
+using TACHYON.Common;
 using TACHYON.Mobile.Dtos;
 
 namespace TACHYON.Mobile
@@ -8,13 +12,15 @@ namespace TACHYON.Mobile
     public class UserDeviceTokenManager : TACHYONDomainServiceBase
     {
         private readonly IRepository<UserDeviceToken> _userDeviceTokenRepository;
-        public UserDeviceTokenManager(IRepository<UserDeviceToken> userDeviceTokenRepository)
+        private readonly TachyonMobileAppSession _mobileAppSession;
+        public UserDeviceTokenManager(IRepository<UserDeviceToken> userDeviceTokenRepository, TachyonMobileAppSession mobileAppSession)
         {
             _userDeviceTokenRepository = userDeviceTokenRepository;
+            _mobileAppSession = mobileAppSession;
         }
         public async Task CreateOrEdit(UserDeviceTokenDto Input)
         {
-            var token = await _userDeviceTokenRepository.FirstOrDefaultAsync(x=> x.UserId==Input.UserId && x.DeviceId== Input.DeviceId);
+            var token = await _userDeviceTokenRepository.FirstOrDefaultAsync(x => x.UserId == Input.UserId && x.DeviceId == Input.DeviceId);
             if (token != null)
                 ObjectMapper.Map(Input, token);
             else
@@ -23,6 +29,20 @@ namespace TACHYON.Mobile
                 await _userDeviceTokenRepository.InsertAsync(token);
             }
         }
-   
+
+        public async Task DeleteUserDeviceToken()
+        {
+            var deviceId = _mobileAppSession.DeviceId;
+            var deviceToken = _mobileAppSession.DeviceToken;
+
+            if (deviceId.IsNullOrEmpty() || deviceToken.IsNullOrEmpty())
+                return;
+
+            var token = await _userDeviceTokenRepository.FirstOrDefaultAsync(x => x.DeviceId == deviceId && x.Token == deviceToken);
+            if (token != null)
+                await _userDeviceTokenRepository.DeleteAsync(token);
+
+        }
+
     }
 }
