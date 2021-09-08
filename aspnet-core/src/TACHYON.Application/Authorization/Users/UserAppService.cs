a6360bf41622ad09d1bd79fe96fcbe2c752deb03
Mosa Alhaj <mosama@trustangle.com>
2021-09-08 15:04:02 +0300

    TAC-1138 Fix Error When Update User/Driver

--- aspnet-core/src/TACHYON.Application/Authorization/Users/UserAppService.cs
+++ aspnet-core/src/TACHYON.Application/Authorization/Users/UserAppService.cs
@@ -14,6 +14,7 @@ using Abp.Runtime.Session;
 using Abp.UI;
 using Abp.Zero.Configuration;
 using AutoMapper.QueryableExtensions;
+using Castle.Core.Internal;
 using DevExtreme.AspNet.Data.ResponseModel;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -344,6 +345,7 @@ namespace TACHYON.Authorization.Users
             //Update user properties
             ObjectMapper.Map(input.User, user); //Passwords is not mapped (see mapping configuration)
 
+
             CheckErrors(await UserManager.UpdateAsync(user));
 
             if (input.SetRandomPassword)
@@ -359,7 +361,12 @@ namespace TACHYON.Authorization.Users
             }
 
             //Update roles
-            CheckErrors(await UserManager.SetRolesAsync(user, input.AssignedRoleNames));
+
+
+            var userRoles = await UserManager.GetRolesAsync(user);
+            if (!input.AssignedRoleNames.Except(userRoles).IsNullOrEmpty()
+                || !userRoles.Except(input.AssignedRoleNames).IsNullOrEmpty())
+                CheckErrors(await UserManager.SetRolesAsync(user, input.AssignedRoleNames));
 
             //update organization units
             await UserManager.SetOrganizationUnitsAsync(user, input.OrganizationUnits.ToArray());
