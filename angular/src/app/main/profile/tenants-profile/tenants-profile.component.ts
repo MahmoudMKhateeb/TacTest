0bbf8667c68f175997e82ca1b3a95aabbbd0e9e0
Mahmoud Kh <malkhateeb@trustangle.com>
2021-09-13 21:53:04 +0300

    TAC-1735 - Shipper Profile Overview

--- angular/src/app/main/profile/tenants-profile/tenants-profile.component.ts
+++ angular/src/app/main/profile/tenants-profile/tenants-profile.component.ts
@@ -1,22 +1,38 @@
-import { Component, Injector, OnInit } from '@angular/core';
+import { Component, Injector, OnInit, ViewEncapsulation } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { AppConsts } from '@shared/AppConsts';
 import { ProfileServiceProxy } from '@shared/service-proxies/service-proxies';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'tenants-profile',
   templateUrl: './tenants-profile.component.html',
   styleUrls: ['./tenants-profile.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class TenantsProfileComponent extends AppComponentBase implements OnInit {
+  public id: number;
   public shownLoginName: string;
   public tenancyName: string;
   public userName: string;
   public email: string;
   public phone: number;
   profilePicture = AppConsts.appBaseUrl + '/assets/common/images/default-profile-picture.png';
+  shipperFacilities = [
+    { id: 1, name: 'Amazon', City: 'Jeddah', long: 46.6791872, lat: 24.7429006 },
+    { id: 2, name: 'Ebay', City: 'Riyadh', long: 50.2082971, lat: 26.3025915 },
+    { id: 3, name: 'Google', City: 'Ajman', long: 50.2082971, lat: 26.3025915 },
+    { id: 4, name: 'Youtube', City: 'Jeddah', long: 46.6791872, lat: 24.7429006 },
+    { id: 5, name: 'Amazon', City: 'Jeddah', long: 50.2082971, lat: 26.3025915 },
+    { id: 6, name: 'Ebay', City: 'Riyadh', long: 46.6791872, lat: 24.7429006 },
+    { id: 7, name: 'Google', City: 'Ajman', long: 50.2082971, lat: 26.3025915 },
+    { id: 8, name: 'Youtube', City: 'Jeddah', long: 46.6791872, lat: 24.7429006 },
+  ];
+  selectedFacility: any;
+  latitude: number;
+  longitude: number;
 
-  constructor(injector: Injector, private _profileServiceProxy: ProfileServiceProxy) {
+  constructor(injector: Injector, private _profileServiceProxy: ProfileServiceProxy, private _Activatedroute: ActivatedRoute) {
     super(injector);
   }
   get isCarrier(): boolean {
@@ -27,9 +43,20 @@ export class TenantsProfileComponent extends AppComponentBase implements OnInit 
   }
 
   ngOnInit(): void {
-    console.log('Profiling Component Is Working');
     this.getProfilePicture();
     this.getCurrentUserInfo();
+    // this._Activatedroute.params.subscribe((params) => {
+    //   console.log('The Tenants Profile Component: ', +params['id']);
+    //   console.log(+params['id']);
+    //   // (+) converts string 'id' to a number
+    //   // In a real app: dispatch action to load the details here.
+    // });
+    this._Activatedroute.params.subscribe((params) => {
+      console.log('The Tenants Profile Component: ', +params['id']);
+      console.log(+params['id']);
+      // (+) converts string 'id' to a number
+      // In a real app: dispatch action to load the details here.
+    });
   }
 
   /**
@@ -56,5 +83,14 @@ export class TenantsProfileComponent extends AppComponentBase implements OnInit 
         this.profilePicture = 'data:image/jpeg;base64,' + result.profilePicture;
       }
     });
+  }
+
+  /**
+   *
+   * Show Facility Location On Map For Shipper
+   */
+  AllocateFacilityOnMap($event) {
+    this.longitude = $event.long;
+    this.latitude = $event.lat;
   }
 }
