aee2aa17ba0186c31c6629a8a4d8a8d421fe5ad2
Mahmoud Kh <malkhateeb@trustangle.com>
2021-09-13 21:54:49 +0300

    TAC-1725 - Update/Edit My Profile Carrier/Shipper

--- angular/src/app/main/profile/complete-profile/complete-profile.component.ts
+++ angular/src/app/main/profile/complete-profile/complete-profile.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { AppConsts } from '@shared/AppConsts';
+import { ProfileServiceProxy } from '@shared/service-proxies/service-proxies';
 
 @Component({
   selector: 'app-complete-profile',
   templateUrl: './complete-profile.component.html',
-  styleUrls: ['./complete-profile.component.css']
+  styleUrls: ['./complete-profile.component.css'],
 })
 export class CompleteProfileComponent implements OnInit {
+  profilePicture = AppConsts.appBaseUrl + '/assets/common/images/default-profile-picture.png';
 
-  constructor() { }
+  constructor(private _profileServiceProxy: ProfileServiceProxy) {}
 
   ngOnInit(): void {
+    this.getProfilePicture();
+    abp.event.on('profilePictureChanged', () => {
+      this.getProfilePicture();
+    });
   }
 
+  /**
+   * get User Profile Picture
+   */
+  getProfilePicture(): void {
+    this._profileServiceProxy.getProfilePicture().subscribe((result) => {
+      if (result && result.profilePicture) {
+        this.profilePicture = 'data:image/jpeg;base64,' + result.profilePicture;
+      }
+    });
+  }
 }
