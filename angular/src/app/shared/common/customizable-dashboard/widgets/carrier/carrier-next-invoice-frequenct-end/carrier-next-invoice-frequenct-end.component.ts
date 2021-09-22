e0524d22f6e9f1d775040c03f60c919bc634b289
Mahmoud Kh <malkhateeb@trustangle.com>
2021-09-22 10:10:52 +0300

    TAC-1806 - Carrier Next Invoice Frequency End Date

---
+++ angular/src/app/shared/common/customizable-dashboard/widgets/carrier/carrier-next-invoice-frequenct-end/carrier-next-invoice-frequenct-end.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carrier-next-invoice-frequenct-end',
+  templateUrl: './carrier-next-invoice-frequenct-end.component.html',
+  styleUrls: ['./carrier-next-invoice-frequenct-end.component.css']
+})
+export class CarrierNextInvoiceFrequenctEndComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
