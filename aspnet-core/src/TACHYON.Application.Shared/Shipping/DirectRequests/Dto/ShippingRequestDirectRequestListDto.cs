4d0129724736c59c85d7d86ef6cc2b827dd0d393
Mosa Alhaj <mosama@trustangle.com>
2021-09-25 19:01:40 +0300

    Add TenantId in DirectRequestListDto

--- aspnet-core/src/TACHYON.Application.Shared/Shipping/DirectRequests/Dto/ShippingRequestDirectRequestListDto.cs
+++ aspnet-core/src/TACHYON.Application.Shared/Shipping/DirectRequests/Dto/ShippingRequestDirectRequestListDto.cs
@@ -3,11 +3,13 @@ using TACHYON.Shipping.ShippingRequests;
 
 namespace TACHYON.Shipping.DirectRequests.Dto
 {
-    public class ShippingRequestDirectRequestListDto: EntityDto<long>
+    public class ShippingRequestDirectRequestListDto : EntityDto<long>
     {
         public string Carrier { get; set; }
+
+        public int TenantId { get; set; }
         public ShippingRequestDirectRequestStatus Status { get; set; }
-        public string StatusTitle { get { return Status.GetEnumDescription();  } }
+        public string StatusTitle { get { return Status.GetEnumDescription(); } }
         public string RejetcReason { get; set; }
     }
 }
