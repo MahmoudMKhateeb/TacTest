3be5ee7757f7c0a3d4bf709ced35f1c293674f4a
islam <ialkhateeb@trustangle.com>
2021-10-14 12:44:52 +0300

    Add DangerousGoodTypeTranslation migration

---
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20211014093353_Create_DangerousGoodTypeTranslation_Table.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace TACHYON.Migrations
+{
+    public partial class Create_DangerousGoodTypeTranslation_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DangerousGoodTypeTranslations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorUserId = table.Column<long>(nullable: true),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    LastModifierUserId = table.Column<long>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeleterUserId = table.Column<long>(nullable: true),
+                    DeletionTime = table.Column<DateTime>(nullable: true),
+                    TranslatedName = table.Column<string>(maxLength: 255, nullable: false),
+                    Language = table.Column<string>(nullable: true),
+                    CoreId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DangerousGoodTypeTranslations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DangerousGoodTypeTranslations_DangerousGoodTypes_CoreId",
+                        column: x => x.CoreId,
+                        principalTable: "DangerousGoodTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DangerousGoodTypeTranslations_CoreId",
+                table: "DangerousGoodTypeTranslations",
+                column: "CoreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DangerousGoodTypeTranslations");
+        }
+    }
+}
