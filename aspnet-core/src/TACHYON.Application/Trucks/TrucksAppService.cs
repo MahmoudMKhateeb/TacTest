95757c0ccae51c9f66579c38bc321b4cc8d9ab82
azhar <a.safi@newsolutions.ps>
2021-11-29 15:21:13 +0200

    TAC-1997  fix out of scope issue

--- aspnet-core/src/TACHYON.Application/Trucks/TrucksAppService.cs
+++ aspnet-core/src/TACHYON.Application/Trucks/TrucksAppService.cs
@@ -106,10 +106,28 @@ namespace TACHYON.Trucks
             DisableTenancyFiltersIfHost();
             await DisableTenancyFiltersIfTachyonDealer();
 
+            var query = (from truck in _truckRepository.GetAll()
+                         join tenant in _lookupTenantRepository.GetAll() on truck.TenantId equals tenant.Id
+                         select new TruckDto()
+                         {
+                             CompanyName = tenant.companyName,
+                             Capacity = truck.Capacity,
+                             CapacityDisplayName = truck.CapacityFk != null ? truck.CapacityFk.DisplayName : "",
+                             ModelName = truck.ModelName,
+                             ModelYear = truck.ModelYear,
+                             Length = truck.Length,
+                             TruckStatusDisplayName = truck.TruckStatusFk != null ? truck.TruckStatusFk.DisplayName : "",
+                             TransportTypeDisplayName = truck.TransportTypeFk != null ? truck.TransportTypeFk.DisplayName : "",
+                             PlateNumber = truck.PlateNumber,
+                             Note = truck.Note,
+                             TransportTypeId = truck.TransportTypeId,
+                             IsMissingDocumentFiles = Convert.ToBoolean(truck.IsAttachable),
+                             TrucksTypeDisplayName = truck.TrucksTypeFk != null ? truck.TrucksTypeFk.DisplayName : "",
+                             CapacityId = truck.CapacityId,
+                             Id = truck.Id,
+                             TrucksTypeId = truck.TrucksTypeId
 
-            var query = _truckRepository.GetAll()
-                .ProjectTo<TruckDto>(AutoMapperConfigurationProvider);
-
+                         });
 
             var result = await LoadResultAsync(query, input.Filter);
             await FillIsMissingDocumentFiles(result);
