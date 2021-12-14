f5855bca6c1d506604ae5fa089bbcf948b4bd31b
Maryana esmail <maryana.2000>
2021-12-14 11:35:40 +0200

    TAC-2154- enable filtering and sorting in Dev table

--- angular/src/app/main/shippingRequests/shippingRequests/shipment-history/shipment-history.component.ts
+++ angular/src/app/main/shippingRequests/shippingRequests/shipment-history/shipment-history.component.ts
@@ -2,7 +2,7 @@ import { Component, Injector, OnInit } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import CustomStore from '@node_modules/devextreme/data/custom_store';
 import { LoadOptions } from '@node_modules/devextreme/data/load_options';
-import { LoadOptionsInput, ShippingRequestsServiceProxy, ShippingRequestType } from '@shared/service-proxies/service-proxies';
+import { LoadOptionsInput, ShippingRequestsServiceProxy, ShippingRequestStatus, ShippingRequestType } from '@shared/service-proxies/service-proxies';
 import { EnumToArrayPipe } from '@shared/common/pipes/enum-to-array.pipe';
 @Component({
   selector: 'app-shipment-history',
@@ -13,7 +13,7 @@ import { EnumToArrayPipe } from '@shared/common/pipes/enum-to-array.pipe';
 export class ShipmentHistoryComponent extends AppComponentBase implements OnInit {
   dataSource: any = {};
   requestTypeData: object[] = [];
-
+  statusData: object[] = [];
   constructor(injector: Injector, private _shippingRequestService: ShippingRequestsServiceProxy, private enumToArray: EnumToArrayPipe) {
     super(injector);
   }
@@ -21,11 +21,11 @@ export class ShipmentHistoryComponent extends AppComponentBase implements OnInit
   ngOnInit(): void {
     this.getAll();
     this.requestTypeData = this.enumToArray.transform(ShippingRequestType);
+    this.statusData = this.enumToArray.transform(ShippingRequestStatus);
   }
 
   getAll() {
     let self = this;
-
     this.dataSource = {};
 
     this.dataSource.store = new CustomStore({
@@ -37,7 +37,12 @@ export class ShipmentHistoryComponent extends AppComponentBase implements OnInit
           .then((response) => {
             return {
               data: response.data,
+
               totalCount: response.totalCount,
+
+              summary: response.summary,
+
+              groupCount: response.groupCount,
             };
           })
           .catch((error) => {
