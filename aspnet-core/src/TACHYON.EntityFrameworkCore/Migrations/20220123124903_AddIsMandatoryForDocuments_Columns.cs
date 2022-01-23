abd71a65a6e7840c494ae9f2e9e06698acebe3ce
azhar <asafi@TRUSTANGLE.COM>
2022-01-23 15:28:01 +0200

    TAC-2427  Add mandatory fiels  for documents trucks and drivers in BE and apply them in components as required fields  in FE

---
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20220123124903_AddIsMandatoryForDocuments_Columns.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TACHYON.Migrations
+{
+    public partial class AddIsMandatoryForDocuments_Columns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRequiredDocumentTemplate",
+                table: "DocumentTypes",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRequiredExpirationDate",
+                table: "DocumentTypes",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRequiredNumber",
+                table: "DocumentTypes",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRequiredDocumentTemplate",
+                table: "DocumentTypes");
+
+            migrationBuilder.DropColumn(
+                name: "IsRequiredExpirationDate",
+                table: "DocumentTypes");
+
+            migrationBuilder.DropColumn(
+                name: "IsRequiredNumber",
+                table: "DocumentTypes");
+        }
+    }
+}
