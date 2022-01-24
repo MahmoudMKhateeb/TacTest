339101b9c5e2bcf8766e4c0ae71449ec9e8fe6b4
tasneem <toudah@trustangle.com>
2022-01-24 11:19:59 +0200

    Solve edit trip error

--- aspnet-core/src/TACHYON.Application/Shipping/Trips/ShippingRequestsTripAppService.cs
+++ aspnet-core/src/TACHYON.Application/Shipping/Trips/ShippingRequestsTripAppService.cs
@@ -460,7 +460,6 @@ namespace TACHYON.Shipping.Trips
             {
                 var trip = await _shippingRequestTripRepository
                 .GetAll()
-                .AsNoTracking()
                 .Include(x => x.ShippingRequestFk)
                 .Include(x => x.OriginFacilityFk)
                 .Include(x => x.DestinationFacilityFk)
