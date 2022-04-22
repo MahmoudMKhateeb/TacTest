5aba03a327823c62de8d879afcfe5e83248c716d
azhar <asafi@TRUSTANGLE.COM>
2022-04-22 20:33:44 +0300

    TAC-3306 Rebase to 1.1.25 , Add pagination for trips in tracking map widget

---
+++ aspnet-core/src/TACHYON.Application.Shared/Dashboards/Shipper/Dto/GetTripsForTrackingInput.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using TACHYON.Dashboards.Shipper.Dto;
+using TACHYON.Routs.RoutPoints;
+using TACHYON.Routs.RoutPoints.Dtos;
+using TACHYON.Shipping.ShippingRequestStatuses;
+using TACHYON.Shipping.Trips;
+
+namespace TACHYON.Dashboards.Shipper.Dto
+{
+    public class GetTripsForTrackingInput : PagedAndSortedResultRequestDto
+    {
+
+    }
+
+}
