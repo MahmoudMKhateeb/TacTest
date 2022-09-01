be7d3e20aa979811661ea4d38c10c942810f4a79
Amjad Almoghrabi <Aalmoghrabi@tachyonhub.com>
2022-09-01 13:23:08 +0300

    TAC-4038 dynamic invoices work in progress

--- angular/src/shared/service-proxies/service-proxy.module.ts
+++ angular/src/shared/service-proxies/service-proxy.module.ts
@@ -157,6 +157,7 @@ import { ZeroTemplateHttpConfigurationService } from './zero-template-http-confi
     ApiServiceProxies.ImportShipmentFromExcelServiceProxy,
     ApiServiceProxies.ShippingRequestAndTripNotesServiceProxy,
     ApiServiceProxies.ShippingRequestUpdateServiceProxy,
+    ApiServiceProxies.DynamicInvoiceServiceProxy,
     { provide: RefreshTokenService, useClass: ZeroRefreshTokenService },
     { provide: AbpHttpConfigurationService, useClass: ZeroTemplateHttpConfigurationService },
     { provide: HTTP_INTERCEPTORS, useClass: AbpHttpInterceptor, multi: true },
