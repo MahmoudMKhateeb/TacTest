561273ea2edf32a05dee4707484e5decb63b8ee5
Mosa Alhaj <mosama@trustangle.com>
2022-09-05 19:25:15 +0300

    Add GetDynamicInvoiceItemInfo

---
+++ aspnet-core/src/TACHYON.Application.Shared/DynamicInvoices/DynamicInvoiceItems/DynamicInvoiceItemLookupDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TACHYON.DynamicInvoices.DynamicInvoiceItems
+{
+    public class DynamicInvoiceItemLookupDto
+    {
+        public string OriginCityName { get; set; }
+
+        public string DestinationCityName { get; set; }
+
+        public DateTime? WorkDate { get; set; }
+
+        public int? Quantity { get; set; }
+
+        public string PlateNumber { get; set; }
+
+        public string ContainerNumber { get; set; }
+    }
+}
