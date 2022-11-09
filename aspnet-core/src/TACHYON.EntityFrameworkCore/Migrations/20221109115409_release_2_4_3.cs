0fc0f9fd0959b776b3036aaf1f13f81680263ba8
Mahmoud KH <malkhateeb@tachyonhub.com>
2022-11-09 13:55:37 +0200

    release 2.4.3 Migrations

---
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20221109115409_release_2_4_3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TACHYON.Migrations
+{
+    public partial class release_2_4_3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ItemName",
+                table: "InvoiceNoteItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentContentType",
+                table: "ActorSubmitInvoices",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DocumentId",
+                table: "ActorSubmitInvoices",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentName",
+                table: "ActorSubmitInvoices",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemName",
+                table: "InvoiceNoteItems");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentContentType",
+                table: "ActorSubmitInvoices");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentId",
+                table: "ActorSubmitInvoices");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentName",
+                table: "ActorSubmitInvoices");
+        }
+    }
+}
