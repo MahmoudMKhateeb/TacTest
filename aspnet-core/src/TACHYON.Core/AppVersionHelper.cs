46bb23ba78b73e06cdd2447037de923f1342c0ed
Mahmoud Kh <malkhateeb@trustangle.com>
2022-11-20 21:00:17 +0200

    update version number to 2.4.9

--- aspnet-core/src/TACHYON.Core/AppVersionHelper.cs
+++ aspnet-core/src/TACHYON.Core/AppVersionHelper.cs
@@ -13,7 +13,7 @@ namespace TACHYON
         /// Gets current version of the application.
         /// It's also shown in the web page.
         /// </summary>
-        public const string Version = "2.4.8";
+        public const string Version = "2.4.9";
 
         /// <summary>
         /// Gets release (last build) date of the application.
