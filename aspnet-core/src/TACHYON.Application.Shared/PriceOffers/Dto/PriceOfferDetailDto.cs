eaf28c70ccc29e833513db827079cfad076d9b04
Tasneem Moh <toudah@tachyonhub.com>
2022-11-24 12:19:15 +0200

    Fix error when set pricing with vases

--- aspnet-core/src/TACHYON.Application.Shared/PriceOffers/Dto/PriceOfferDetailDto.cs
+++ aspnet-core/src/TACHYON.Application.Shared/PriceOffers/Dto/PriceOfferDetailDto.cs
@@ -5,7 +5,7 @@ namespace TACHYON.PriceOffers.Dto
     public class PriceOfferDetailDto
     {
         public long ItemId { get; set; }
-        public int Price { get; set; }
+        public int? Price { get; set; }
         [JsonIgnore] public decimal? CommissionPercentageOrAddValue { get; set; }
         [JsonIgnore] public PriceOfferType PriceType { get; set; } = PriceOfferType.Vas;
         [JsonIgnore] public PriceOfferCommissionType CommissionType { get; set; }
