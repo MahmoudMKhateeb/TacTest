ce23cd2096a2f7309bb33ebc212d181b2bdbd297
Islam Al Khatib <Ialkhateeb@trustangle.com>
2022-11-27 13:13:55 +0200

    reset migrations to v 2.4.13

---
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20221127110607_release_2_4_13.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TACHYON.Migrations
+{
+    public partial class release_2_4_13 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
