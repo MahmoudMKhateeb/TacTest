c6cea72dccc61881c90c0384713100564ab745a1
Tasneem Moh <toudah@tachyonhub.com>
2022-12-06 11:27:27 +0200

    Ability to download and see details of drafted invoice

--- aspnet-core/src/TACHYON.Application/Invoices/InvoiceAppService.cs
+++ aspnet-core/src/TACHYON.Application/Invoices/InvoiceAppService.cs
@@ -571,7 +571,7 @@ namespace TACHYON.Invoices
             var bankNameEnglish = SettingManager.GetSettingValue(AppSettings.Invoice.BankNameEnglish);
 
             DisableTenancyFilters();
-            DisableDraftedFilter();
+            AsyncHelper.RunSync(() => DisableInvoiceDraftedFilter());
             var invoice = _invoiceRepository
                 .GetAll()
                 .Include(i => i.InvoicePeriodsFK)
