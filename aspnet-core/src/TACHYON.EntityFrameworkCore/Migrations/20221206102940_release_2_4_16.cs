95f84cfb14867f6e7dfc9cfcbae3436574fecd86
Mahmoud Kh <malkhateeb@trustangle.com>
2022-12-06 12:30:52 +0200

    Release "2.4.16" Migrations

---
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20221206102940_release_2_4_16.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TACHYON.Migrations
+{
+    public partial class release_2_4_16 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "DriverUserId",
+                table: "DedicatedShippingRequestTrucks",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DriverUserId",
+                table: "DedicatedShippingRequestTrucks");
+        }
+    }
+}
