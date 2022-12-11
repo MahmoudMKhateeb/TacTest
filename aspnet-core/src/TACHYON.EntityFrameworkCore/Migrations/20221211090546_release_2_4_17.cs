757ca3a73050681a45355a72d295d0b51316ef10
Mahmoud <mangozaaz@outlook.com>
2022-12-11 21:01:08 +0900

    release 2.4.17 migrations

---
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20221211090546_release_2_4_17.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TACHYON.Migrations
+{
+    public partial class release_2_4_17 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AllNumberDays",
+                table: "DedicatedDynamicInvoiceItems",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllNumberDays",
+                table: "DedicatedDynamicInvoiceItems");
+        }
+    }
+}
