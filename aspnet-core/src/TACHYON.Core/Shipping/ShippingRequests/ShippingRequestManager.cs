e38aff5ecac0b9f389b491e05e31e962efe0eb8d
Mosa Alhaj <mosama@trustangle.com>
2022-12-15 18:06:16 +0200

    TAC-4839 fix can't assign driver & truck

--- aspnet-core/src/TACHYON.Core/Shipping/ShippingRequests/ShippingRequestManager.cs
+++ aspnet-core/src/TACHYON.Core/Shipping/ShippingRequests/ShippingRequestManager.cs
@@ -480,7 +480,8 @@ namespace TACHYON.Shipping.ShippingRequests
         {
             DisableTenancyFilters();
             var item = await _dedicatedShippingRequestTrucksRepository.GetAll()
-                .Where(x => x.TruckId == truckId && x.ShippingRequestId != trip.ShippingRequestId && x.ReplacementFlag == ReplacementFlag.Original &&
+                .Where(x => x.TruckId == truckId && x.ShippingRequestId != trip.ShippingRequestId && x.ReplacementFlag == ReplacementFlag.Original && 
+                            x.ShippingRequest.EndTripDate.HasValue && x.ShippingRequest.RentalStartDate.HasValue && x.ShippingRequest.RentalEndDate.HasValue && trip.EndTripDate.HasValue &&
                 trip.StartTripDate.Date <= x.ShippingRequest.RentalEndDate.Value.Date &&
                 x.ShippingRequest.RentalStartDate.Value.Date <= trip.EndTripDate.Value.Date)
                 .FirstOrDefaultAsync();
@@ -493,6 +494,7 @@ namespace TACHYON.Shipping.ShippingRequests
             DisableTenancyFilters();
             var item = await _dedicatedShippingRequestDriverRepository.GetAll()
                 .Where(x => x.DriverUserId == driverId && x.ShippingRequestId != trip.ShippingRequestId && x.ReplacementFlag == ReplacementFlag.Original &&
+                            x.ShippingRequest.EndTripDate.HasValue && x.ShippingRequest.RentalStartDate.HasValue && x.ShippingRequest.RentalEndDate.HasValue && trip.EndTripDate.HasValue &&
                 trip.StartTripDate.Date <= x.ShippingRequest.RentalEndDate.Value.Date &&
                 x.ShippingRequest.RentalStartDate.Value.Date <= trip.EndTripDate.Value.Date)
                 .FirstOrDefaultAsync();
