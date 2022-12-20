1eb24070e8545e3e8ee688b6353888e2ca479820
Mahmoud <mangozaaz@outlook.com>
2022-12-20 11:02:48 +0200

    update Version to "2.4.22" and add migrations

---
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20221220090117_release_2_4_22.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TACHYON.Migrations
+{
+    public partial class release_2_4_22 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ShippingTypeId",
+                table: "TmsPricePackages",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TmsPricePackages_ShippingTypeId",
+                table: "TmsPricePackages",
+                column: "ShippingTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TmsPricePackages_ShippingTypes_ShippingTypeId",
+                table: "TmsPricePackages",
+                column: "ShippingTypeId",
+                principalTable: "ShippingTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TmsPricePackages_ShippingTypes_ShippingTypeId",
+                table: "TmsPricePackages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TmsPricePackages_ShippingTypeId",
+                table: "TmsPricePackages");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingTypeId",
+                table: "TmsPricePackages");
+        }
+    }
+}
