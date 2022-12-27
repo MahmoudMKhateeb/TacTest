54f0aa40948c1493e2b5bfcf9c8430188439cfef
Mahmoud <mangozaaz@outlook.com>
2022-12-27 22:24:09 +0900

    update migrations file to 2.4.26

--- aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20221227132256_2.4.26.Designer.cs
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20221227132256_2.4.26.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using TACHYON.EntityFrameworkCore;
@@ -10,9 +11,10 @@ using TACHYON.EntityFrameworkCore;
 namespace TACHYON.Migrations
 {
     [DbContext(typeof(TACHYONDbContext))]
-    partial class TACHYONDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221227132256_2.4.26")]
+    partial class _2426
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
