71933e3f680c033f78155ca063d7053ae4f55fd8
Mosa Alhaj <mosama@trustangle.com>
2023-01-18 14:32:44 +0200

    TAC-4407 add calculate price by matching price packages

--- angular/src/app/main/pricePackages/tms-price-package/create-tms-price-package-modal/create-tms-price-package-modal.component.ts
+++ angular/src/app/main/pricePackages/tms-price-package/create-tms-price-package-modal/create-tms-price-package-modal.component.ts
@@ -2,8 +2,11 @@ import { Component, EventEmitter, Injector, OnInit, Output, ViewChild } from '@a
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import {
+  CarriersForDropDownDto,
   CreateOrEditTmsPricePackageDto,
+  NormalPricePackageDto,
   NormalPricePackagesServiceProxy,
+  PriceOfferCommissionType,
   PricePackageType,
   SelectItemDto,
   ShippingRequestRouteType,
@@ -14,6 +17,10 @@ import { EnumToArrayPipe } from '@shared/common/pipes/enum-to-array.pipe';
 import CustomStore from '@node_modules/devextreme/data/custom_store';
 import { LoadOptions } from '@node_modules/devextreme/data/load_options';
 import { DxDataGridComponent } from '@node_modules/devextreme-angular';
+import { TmsPricePackagePricingMethod } from '@app/main/pricePackages/tms-price-package/create-tms-price-package-modal/tms-price-package-pricing-method';
+import { TmsPricePackageCommissionType } from '@app/main/pricePackages/tms-price-package/create-tms-price-package-modal/tms-price-package-commission-type';
+import { isNotNullOrUndefined } from '@node_modules/codelyzer/util/isNotNullOrUndefined';
+import { finalize } from '@node_modules/rxjs/operators';
 
 @Component({
   selector: 'app-create-tms-price-package-modal',
@@ -22,7 +29,7 @@ import { DxDataGridComponent } from '@node_modules/devextreme-angular';
 })
 export class CreateTmsPricePackageModalComponent extends AppComponentBase implements OnInit {
   @ViewChild('Modal') modal: ModalDirective;
-  @ViewChild('dataGrid', { static: true }) dataGrid: DxDataGridComponent;
+  @ViewChild('grid', { static: true }) dataGrid: DxDataGridComponent;
   @Output() modalSave: EventEmitter<any> = new EventEmitter<any>();
 
   tmsPricePackage: CreateOrEditTmsPricePackageDto;
@@ -38,6 +45,21 @@ export class CreateTmsPricePackageModalComponent extends AppComponentBase implem
   routeTypes = this._enumToArrayPipe.transform(ShippingRequestRouteType);
   dataSource: any = {};
   shippingTypes: SelectItemDto[];
+  pricingMethod: TmsPricePackagePricingMethod;
+  pricingMethodEnum = TmsPricePackagePricingMethod;
+  pricingMethods;
+  calculatedPriceTitle: string;
+  commissionType: TmsPricePackageCommissionType;
+  commissionTypeEnum = TmsPricePackageCommissionType;
+  commissionAmount: number;
+  commissionTypes;
+  calculatedPrice: number;
+  totalCommission: number;
+  carriersLoading: boolean;
+  carriers: CarriersForDropDownDto[];
+  selectedCarriers: CarriersForDropDownDto[];
+  selectedRows: NormalPricePackageDto[] = [];
+  totalPrice: number;
 
   constructor(
     private injector: Injector,
@@ -54,8 +76,13 @@ export class CreateTmsPricePackageModalComponent extends AppComponentBase implem
     this.isFormActive = false;
     this.isFormSaving = false;
     this.loadAllDropDowns();
-    this.getAllNormalPricePackages();
     this.getAllShippingType();
+    this.loadAllCarriers();
+    this.pricingMethods = this._enumToArrayPipe.transform(this.pricingMethodEnum);
+    // The Pricing Method default value is `Average`
+    this.pricingMethod = TmsPricePackagePricingMethod.Average;
+    this.commissionTypes = this._enumToArrayPipe.transform(TmsPricePackageCommissionType);
+    this.calculatedPriceTitle = '';
   }
 
   show(id?: number): void {
@@ -68,6 +95,8 @@ export class CreateTmsPricePackageModalComponent extends AppComponentBase implem
         this.isFormLoading = false;
       });
     }
+    this.pricingMethod = TmsPricePackagePricingMethod.Average;
+    this.calculatedPriceTitle = this.l('AveragePrice');
     this.modal.show();
     this.isFormActive = true;
   }
@@ -128,6 +157,16 @@ export class CreateTmsPricePackageModalComponent extends AppComponentBase implem
     this.isFormActive = false;
     this.isFormSaving = false;
     this.truckTypes = undefined;
+    this.pricingMethod = undefined;
+    this.calculatedPriceTitle = undefined;
+    this.commissionType = undefined;
+    this.commissionAmount = undefined;
+    this.calculatedPrice = undefined;
+    this.totalPrice = undefined;
+    this.totalCommission = undefined;
+    this.selectedCarriers = undefined;
+    this.selectedRows = undefined;
+
     this.modal.hide();
   }
 
@@ -135,11 +174,19 @@ export class CreateTmsPricePackageModalComponent extends AppComponentBase implem
    * loads All Normal Price Packeges For View Table
    * @private
    */
-  private getAllNormalPricePackages(): void {
+  private getAllNormalPricePackages(filter: any[] | undefined): void {
     let self = this;
     this.dataSource = {};
     this.dataSource.store = new CustomStore({
       load(loadOptions: LoadOptions) {
+        if (isNotNullOrUndefined(filter)) {
+          if (isNotNullOrUndefined(loadOptions.filter)) {
+            loadOptions.filter.push(filter);
+          } else {
+            loadOptions.filter = filter;
+          }
+        }
+        console.log(loadOptions.filter);
         return self._normalPricePackagesServiceProxy
           .getAll(JSON.stringify(loadOptions))
           .toPromise()
@@ -160,6 +207,11 @@ export class CreateTmsPricePackageModalComponent extends AppComponentBase implem
 
   public createOrEdit(): void {
     this.isFormSaving = true;
+    if (this.pricingMethod == this.pricingMethodEnum.FinalPrice) {
+      this.tmsPricePackage.totalPrice = this.calculatedPrice;
+    } else {
+      this.tmsPricePackage.totalPrice = this.totalPrice;
+    }
     this._tmsPricePackagesServiceProxy.createOrEdit(this.tmsPricePackage).subscribe(() => {
       this.notify.success(this.l('Success'));
       this.isFormSaving = false;
@@ -172,5 +224,163 @@ export class CreateTmsPricePackageModalComponent extends AppComponentBase implem
     this._shippingRequestServiceProxy.getAllShippingTypesForDropdown().subscribe((result) => {
       this.shippingTypes = result;
     });
+  }
+
+  calculatePriceForSelection() {
+    let prices = this.selectedRows.map((dto, index, array) => {
+      return dto.tachyonMSRequestPrice;
+    });
+
+    if (this.pricingMethod == this.pricingMethodEnum.Average) {
+      this.calculateAveragePrice(prices);
+    }
+
+    if (this.pricingMethod == this.pricingMethodEnum.Maximum) {
+      this.calculateMaximumPrice(prices);
+    }
+
+    if (this.pricingMethod == this.pricingMethodEnum.Minimum) {
+      this.calculateMinimumPrice(prices);
+    }
+
+    if (this.pricingMethod != this.pricingMethodEnum.FinalPrice) {
+      this.calculateTotalCommissionAndTotalPrice();
+    }
+    // case TmsPricePackagePricingMethod.FinalPrice:
+    // don't do anything ( in final price method ) we add the price manually so no need to calculation
+  }
+
+  updatePricingMethod() {
+    if (this.pricingMethod == this.pricingMethodEnum.Average) {
+      this.calculatedPriceTitle = this.l('AveragePrice');
+    }
+
+    if (this.pricingMethod == this.pricingMethodEnum.Maximum) {
+      this.calculatedPriceTitle = this.l('MaximumPrice');
+    }
+
+    if (this.pricingMethod == this.pricingMethodEnum.Minimum) {
+      this.calculatedPriceTitle = this.l('MinimumPrice');
+    }
+
+    if (this.pricingMethod == this.pricingMethodEnum.FinalPrice) {
+      this.calculatedPriceTitle = this.l('FinalPrice');
+      this.calculatedPrice = undefined;
+    }
+
+    this.calculatePriceForSelection();
+  }
+
+  calculateTotalCommissionAndTotalPrice() {
+    if (!isNotNullOrUndefined(this.commissionType)) {
+      return;
+    }
+
+    if (this.commissionType == this.commissionTypeEnum.Percentage) {
+      this.totalCommission = (this.calculatedPrice * this.commissionAmount) / 100;
+    }
+
+    if (this.commissionType == this.commissionTypeEnum.AddValue) {
+      this.totalCommission = this.commissionAmount;
+    }
+
+    this.totalPrice = this.totalCommission + this.calculatedPrice;
+  }
+
+  applyFilters() {
+    let carriersFilter = this.buildCarriersFilter();
+    let pricePackageFilter = this.buildPricePackageFilter();
+
+    let filter = [];
+    if (isNotNullOrUndefined(carriersFilter) && carriersFilter.length > 0) {
+      filter.push(carriersFilter);
+    }
+
+    if (isNotNullOrUndefined(pricePackageFilter) && pricePackageFilter.length > 0) {
+      if (filter.length > 0) {
+        filter.push('and');
+      }
+      filter.push(pricePackageFilter);
+    }
+
+    this.getAllNormalPricePackages(filter);
+  }
+
+  private loadAllCarriers(): void {
+    this.carriersLoading = true;
+    this._shippingRequestServiceProxy
+      .getAllCarriersForDropDown()
+      .pipe(finalize(() => (this.carriersLoading = false)))
+      .subscribe((res) => {
+        this.carriers = res;
+      });
+  }
+
+  private calculateAveragePrice(prices: number[]) {
+    if (prices.length === 0) {
+      this.calculatedPrice = 0;
+      return;
+    }
+
+    let total = prices.reduce((last, next) => last + next, 0);
+
+    this.calculatedPrice = total / prices.length;
+  }
+
+  private calculateMaximumPrice(prices: number[]) {
+    this.calculatedPrice = Math.max(...prices);
+  }
+
+  private calculateMinimumPrice(prices: number[]) {
+    this.calculatedPrice = Math.min(...prices);
+  }
+
+  private buildCarriersFilter(): any[] {
+    let filter = [];
+
+    if (!isNotNullOrUndefined(this.selectedCarriers)) {
+      return filter;
+    }
+
+    for (let i = 0; i < this.selectedCarriers.length; i++) {
+      if (i > 0) {
+        filter.push('or');
+      }
+      filter.push(['TenantId', '=', this.selectedCarriers[i].id]);
+    }
+
+    return filter;
+  }
+
+  private buildPricePackageFilter(): any[] {
+    let filter = [];
+
+    if (isNotNullOrUndefined(this.tmsPricePackage.transportTypeId)) {
+      filter.push(['transportTypeId', '=', this.tmsPricePackage.transportTypeId]);
+    }
+
+    if (isNotNullOrUndefined(this.tmsPricePackage.trucksTypeId)) {
+      if (filter.length > 0) {
+        filter.push('and');
+      }
+      filter.push(['trucksTypeId', '=', this.tmsPricePackage.trucksTypeId]);
+    }
+
+    if (isNotNullOrUndefined(this.tmsPricePackage.originCityId)) {
+      if (filter.length > 0) {
+        filter.push('and');
+      }
+
+      filter.push(['originCityId', '=', this.tmsPricePackage.originCityId]);
+    }
+
+    if (isNotNullOrUndefined(this.tmsPricePackage.destinationCityId)) {
+      if (filter.length > 0) {
+        filter.push('and');
+      }
+      filter.push(['destinationCityId', '=', this.tmsPricePackage.destinationCityId]);
+    }
+
+    return filter;
   }
 }
