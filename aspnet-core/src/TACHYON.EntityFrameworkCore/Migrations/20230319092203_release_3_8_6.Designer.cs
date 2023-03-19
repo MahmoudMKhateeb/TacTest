5c037d7e3faec6ad72cf6eb575762411ca548077
Mahmoud <mangozaaz@outlook.com>
2023-03-19 11:22:48 +0200

    update migrations files for test branch "3.8.6"

--- aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20230319092203_release_3_8_6.Designer.cs
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20230319092203_release_3_8_6.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using TACHYON.EntityFrameworkCore;
@@ -10,9 +11,10 @@ using TACHYON.EntityFrameworkCore;
 namespace TACHYON.Migrations
 {
     [DbContext(typeof(TACHYONDbContext))]
-    partial class TACHYONDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230319092203_release_3_8_6")]
+    partial class release_3_8_6
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20230319092203_release_3_8_6.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TACHYON.Migrations
+{
+    public partial class release_3_8_6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "PricePackageId",
+                table: "ServiceAreas",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OriginCountryId",
+                table: "PricePackages",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProjectName",
+                table: "PricePackages",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ScopeOfWork",
+                table: "PricePackages",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceAreas_PricePackageId",
+                table: "ServiceAreas",
+                column: "PricePackageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServiceAreas_PricePackages_PricePackageId",
+                table: "ServiceAreas",
+                column: "PricePackageId",
+                principalTable: "PricePackages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceAreas_PricePackages_PricePackageId",
+                table: "ServiceAreas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ServiceAreas_PricePackageId",
+                table: "ServiceAreas");
+
+            migrationBuilder.DropColumn(
+                name: "PricePackageId",
+                table: "ServiceAreas");
+
+            migrationBuilder.DropColumn(
+                name: "OriginCountryId",
+                table: "PricePackages");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectName",
+                table: "PricePackages");
+
+            migrationBuilder.DropColumn(
+                name: "ScopeOfWork",
+                table: "PricePackages");
+        }
+    }
+}
