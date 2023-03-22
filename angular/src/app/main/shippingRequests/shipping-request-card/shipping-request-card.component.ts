c826e908f881559f7d2b55a5a8ebafcde4e2c535
Amjad Almoghrabi <Aalmoghrabi@tachyonhub.com>
2023-03-22 10:03:27 +0200

    TAC-5353 My shipping request page enhancment

---
+++ angular/src/app/main/shippingRequests/shipping-request-card/shipping-request-card.component.ts
@@ -0,0 +1,171 @@
+import { Component, EventEmitter, Injector, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
+import { AppComponentBase } from '@shared/common/app-component-base';
+import {
+  GetShippingRequestForPriceOfferListDto,
+  PriceOfferChannel,
+  ShippingRequestDirectRequestStatus,
+  ShippingRequestFlag,
+  ShippingRequestRouteType,
+  ShippingRequestStatus,
+  ShippingRequestType,
+  ShippingTypeEnum,
+} from '@shared/service-proxies/service-proxies';
+import { Router } from '@angular/router';
+import { ShippingrequestsDetailsModelComponent } from '@app/main/shippingRequests/shippingRequests/details/shippingrequests-details-model.component';
+import { AssignTrucksAndDriversModalComponent } from '@app/main/shippingRequests/shippingRequests/request-templates/assign-trucks-and-drivers-modal/assign-trucks-and-drivers-modal.component';
+import { ReplaceTrucksAndDriversModalComponent } from '@app/main/shippingRequests/shippingRequests/request-templates/replace-trucks-and-drivers-modal/replace-trucks-and-drivers-modal.component';
+import { DedicatedShippingRequestAttendanceSheetModalComponent } from '@app/main/shippingRequests/dedicatedShippingRequest/dedicated-shipping-request-attendance-sheet-modal/dedicated-shipping-request-attendance-sheet-modal.component';
+
+@Component({
+  selector: 'app-shipping-request-card',
+  templateUrl: './shipping-request-card.component.html',
+  styleUrls: ['./shipping-request-card.component.scss'],
+  animations: [appModuleAnimation()],
+})
+export class ShippingRequestCardComponent extends AppComponentBase implements OnInit {
+  // @ViewChild('Model', { static: false }) modalMore: ShippingrequestsDetailsModelComponent;
+  // @ViewChild('assignTrucksAndDriversModal', { static: false }) assignTrucksAndDriversModal: AssignTrucksAndDriversModalComponent;
+  // @ViewChild('replaceTrucksAndDriversModal', { static: false }) replaceTrucksAndDriversModal: ReplaceTrucksAndDriversModalComponent;
+  // @ViewChild('attendanceModal', { static: true }) attendanceModal: DedicatedShippingRequestAttendanceSheetModalComponent;
+
+  @Output() onSearch: EventEmitter<any> = new EventEmitter<any>();
+  @Output() onLoadData: EventEmitter<any> = new EventEmitter<any>();
+  @Output() onDelete: EventEmitter<GetShippingRequestForPriceOfferListDto> = new EventEmitter<GetShippingRequestForPriceOfferListDto>();
+  @Output() onDecline: EventEmitter<GetShippingRequestForPriceOfferListDto> = new EventEmitter<GetShippingRequestForPriceOfferListDto>();
+
+  @Input() item: GetShippingRequestForPriceOfferListDto;
+  @Input() Channel: PriceOfferChannel | number | null | undefined = undefined;
+
+  PriceOfferChannelEnum = PriceOfferChannel;
+  ShippingRequestFlagEnum = ShippingRequestFlag;
+  shippingRequestStatusEnum = ShippingRequestStatus;
+  ShippingTypeEnum = ShippingTypeEnum;
+  ShippingRequestRouteType = ShippingRequestRouteType;
+
+  constructor(injector: Injector, private router: Router) {
+    super(injector);
+  }
+
+  ngOnInit() {}
+
+  setTitle(item: GetShippingRequestForPriceOfferListDto): string {
+    if (this.Channel === PriceOfferChannel.DirectRequest) {
+      if (this.feature.isEnabled('App.Carrier')) {
+        return item.isTachyonDeal ? this.l('TachyonManageService') : item.name;
+      }
+      if (this.feature.isEnabled('App.Shipper') || this.feature.isEnabled('App.TachyonDealer') || !this.appSession.tenantId) {
+        return item.name;
+      }
+    } else if (this.Channel === PriceOfferChannel.MarketPlace) {
+      if (this.feature.isEnabled('App.Carrier') || this.feature.isEnabled('App.TachyonDealer') || !this.appSession.tenantId) {
+        return item.isTachyonDeal ? this.l('TachyonManageService') : item.name;
+      }
+    } else if (this.Channel === PriceOfferChannel.Offers) {
+      if (this.feature.isEnabled('App.Carrier') || this.feature.isEnabled('App.TachyonDealer') || !this.appSession.tenantId) {
+        return item.isTachyonDeal ? this.l('TachyonManageService') : item.name;
+      }
+    } /*Shipping request page*/ else {
+      if (this.feature.isEnabled('App.Carrier')) {
+        return item.isTachyonDeal ? this.l('TachyonManageService') : item.name;
+      }
+      if (this.feature.isEnabled('App.TachyonDealer') || !this.appSession.tenantId) {
+        if (item.status === ShippingRequestStatus.PostPrice || item.status === ShippingRequestStatus.Completed) {
+          return `${item.name} - ${item.carrier}`;
+        } else {
+          return item.name;
+        }
+      }
+    }
+    return '';
+  }
+
+  getWordTitle(n: any, word: string): string {
+    if (parseInt(n) === 1) {
+      return this.l(word);
+    }
+    return this.l(`${word}s`);
+  }
+
+  canSeeTotalOffers(item: GetShippingRequestForPriceOfferListDto) {
+    if (item.price > 0) {
+      return false;
+    }
+    if (item.totalOffers > 0 && !this.isCarrier && this.Channel != 2 && this.Channel != 10 && item.status == 2) {
+      return true;
+    }
+    return false;
+  }
+
+  search(): void {
+    this.onSearch.emit(true);
+  }
+
+  LoadData(): void {
+    this.onLoadData.emit(true);
+  }
+
+  canViewBrokerPrice(item: GetShippingRequestForPriceOfferListDto): boolean {
+    if ((item.shipperActor || item.carrierActor) && !this.isTachyonDealerOrHost) {
+      return false;
+    }
+
+    return true;
+  }
+
+  canDeleteDirectRequest(input: GetShippingRequestForPriceOfferListDto) {
+    if (
+      this.Channel === PriceOfferChannel.DirectRequest &&
+      (input.directRequestStatus === ShippingRequestDirectRequestStatus.New ||
+        input.directRequestStatus === ShippingRequestDirectRequestStatus.Declined)
+    ) {
+      if ((this.isTachyonDealer && input.isTachyonDeal) || (this.isShipper && !input.isTachyonDeal)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // delete(input: GetShippingRequestForPriceOfferListDto): void {
+  //     this.onDelete.emit(input);
+  // }
+  //
+  // decline(input: GetShippingRequestForPriceOfferListDto): void {
+  //     this.onDecline.emit(input);
+  // }
+
+  // moreRedirectTo(item: GetShippingRequestForPriceOfferListDto): void {
+  //     if (!this.Channel && this.appSession.tenantId) {
+  //         if (
+  //             !this.feature.isEnabled('App.TachyonDealer') ||
+  //             (this.feature.isEnabled('App.TachyonDealer') && item.requestType === ShippingRequestType.TachyonManageService) ||
+  //             (this.feature.isEnabled('App.TachyonDealer') && item.shippingRequestFlag === ShippingRequestFlag.Dedicated)
+  //         ) {
+  //             this.router.navigateByUrl(`/app/main/shippingRequests/shippingRequests/view?id=${item.id}`);
+  //             return;
+  //         }
+  //     }
+  //     this.modalMore.show(item);
+  // }
+
+  isCarrierOwnRequest(request: GetShippingRequestForPriceOfferListDto): boolean {
+    return this.isCarrierSaas && request.isSaas && this.appSession.tenantId === request.tenantId;
+  }
+
+  // assignTrucksAndDrivers(item: GetShippingRequestForPriceOfferListDto) {
+  //     console.log('item', item);
+  //     this.assignTrucksAndDriversModal.show(item);
+  // }
+
+  // viewTrucksOrDrivers(item: GetShippingRequestForPriceOfferListDto, isForTruck: boolean) {
+  //     console.log('item', item);
+  //     this.replaceTrucksAndDriversModal.show(item, isForTruck);
+  // }
+  //
+  // openAttendanceModal() {
+  //     this.attendanceModal.show(this.item.status === this.shippingRequestStatusEnum.Completed, null, this.item.id, {
+  //         rentalStartDate: this.item?.rentalStartDate,
+  //         rentalEndDate: this.item?.rentalEndDate,
+  //     });
+  // }
+}
