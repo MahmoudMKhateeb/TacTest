7d2b93935c12beec83313671b2594c5e52ab8f53
Amjad Almoghrabi <Aalmoghrabi@tachyonhub.com>
2023-06-05 11:54:31 +0300

    TAC-5629 Cost Vs Selling Vs Profit of SaaS Trips FE link to api

--- angular/src/app/shared/common/customizable-dashboard/widgets/host/cost-vs-selling-vs-profit-of-saas-trips/cost-vs-selling-vs-profit-of-saas-trips.component.ts
+++ angular/src/app/shared/common/customizable-dashboard/widgets/host/cost-vs-selling-vs-profit-of-saas-trips/cost-vs-selling-vs-profit-of-saas-trips.component.ts
@@ -1,6 +1,6 @@
 import { Component, Injector, OnInit, ViewChild } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
-import { HostDashboardServiceProxy } from '@shared/service-proxies/service-proxies';
+import { HostDashboardServiceProxy, TMSAndHostDashboardServiceProxy } from '@shared/service-proxies/service-proxies';
 import {
   ApexAxisChartSeries,
   ApexChart,
@@ -14,6 +14,7 @@ import {
   ApexFill,
   ApexTooltip,
 } from 'ng-apexcharts';
+import { finalize } from 'rxjs/operators';
 
 export interface ChartOptions {
   series: ApexAxisChartSeries;
@@ -38,64 +39,76 @@ export class CostVsSellingVsProfitOfSaasTripsComponent extends AppComponentBase 
   public chartOptions: Partial<ChartOptions>;
 
   loading = false;
-  constructor(private injector: Injector, private _hostDashboardServiceProxy: HostDashboardServiceProxy) {
+  constructor(private injector: Injector, private _TMSAndHostDashboardServiceProxy: TMSAndHostDashboardServiceProxy) {
     super(injector);
   }
 
-  ngOnInit(): void {
-    this.chartOptions = {
-      series: [
-        {
-          name: this.l('Cost'),
-          data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
-          color: '#dc2434',
-        },
-        {
-          name: this.l('Selling'),
-          data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
-          color: '#000',
-        },
-        {
-          name: this.l('Profit'),
-          data: [55, 57, 40, 20, 30, 11, 60, 90, 75],
-          color: '#707070',
-        },
-      ],
-      chart: {
-        type: 'bar',
-        height: '100%',
-        width: '100%',
-      },
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          columnWidth: '55%',
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        show: true,
-        width: 2,
-        colors: ['transparent'],
-      },
-      xaxis: {
-        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-      },
-      yaxis: {
-        title: {},
-      },
-      fill: {
-        opacity: 1,
-      },
-      tooltip: {
-        y: {},
-      },
-    };
+  ngOnInit(): void {}
+
+  getData(event: { start: moment.Moment; end: moment.Moment }) {
+    this.loading = true;
+    this._TMSAndHostDashboardServiceProxy
+      .getCostVsSellingSAASTrips(event.start, event.end)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+        })
+      )
+      .subscribe((res) => {
+        this.chartOptions = {
+          series: [
+            {
+              name: this.l('Cost'),
+              data: res.cost.map((item) => item.y),
+              color: '#dc2434',
+            },
+            {
+              name: this.l('Selling'),
+              data: res.selling.map((item) => item.y),
+              color: '#000',
+            },
+            {
+              name: this.l('Profit'),
+              data: res.profit.map((item) => item.y),
+              color: '#707070',
+            },
+          ],
+          chart: {
+            type: 'bar',
+            height: '100%',
+            width: '100%',
+          },
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              columnWidth: '55%',
+            },
+          },
+          dataLabels: {
+            enabled: false,
+          },
+          stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent'],
+          },
+          xaxis: {
+            categories: res.cost.map((item) => item.x),
+          },
+          yaxis: {
+            title: {},
+          },
+          fill: {
+            opacity: 1,
+          },
+          tooltip: {
+            y: {},
+          },
+        };
+      });
   }
 
-  getData() {
-    this.loading = true;
+  selectedFilter(event: { start: moment.Moment; end: moment.Moment }) {
+    this.getData(event);
   }
 }
