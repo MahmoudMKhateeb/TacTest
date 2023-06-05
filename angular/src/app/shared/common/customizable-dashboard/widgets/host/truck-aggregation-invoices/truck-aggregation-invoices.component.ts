09d541e161e6bc1493e89d6878140e78f5cb5144
Amjad Almoghrabi <Aalmoghrabi@tachyonhub.com>
2023-06-05 12:59:55 +0300

    TAC-5618 Truck Aggregation Invoices FE link to api

--- angular/src/app/shared/common/customizable-dashboard/widgets/host/truck-aggregation-invoices/truck-aggregation-invoices.component.ts
+++ angular/src/app/shared/common/customizable-dashboard/widgets/host/truck-aggregation-invoices/truck-aggregation-invoices.component.ts
@@ -1,5 +1,7 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
+import { TMSAndHostDashboardServiceProxy } from '@shared/service-proxies/service-proxies';
+import { finalize } from '@node_modules/rxjs/operators';
 
 @Component({
   selector: 'app-truck-aggregation-invoices',
@@ -9,11 +11,11 @@ import { AppComponentBase } from '@shared/common/app-component-base';
 export class TruckAggregationInvoicesComponent extends AppComponentBase implements OnInit {
   loading = false;
 
-  selling = 20000;
-  cost = 15000;
-  profit = 5000;
+  selling = 0;
+  cost = 0;
+  profit = 0;
 
-  constructor(private injector: Injector) {
+  constructor(private injector: Injector, private _TMSAndHostDashboardServiceProxy: TMSAndHostDashboardServiceProxy) {
     super(injector);
   }
 
@@ -22,6 +24,18 @@ export class TruckAggregationInvoicesComponent extends AppComponentBase implemen
   }
 
   getData() {
-    // this.loading = true;
+    this.loading = true;
+    this._TMSAndHostDashboardServiceProxy
+      .getTruckAggregationInvoicesCostAndSelling()
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+        })
+      )
+      .subscribe((res) => {
+        this.selling = res.selling;
+        this.cost = res.cost;
+        this.profit = res.profit;
+      });
   }
 }
