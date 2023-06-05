512d109933c5b6975359e07e5eb075109c46e2b4
Amjad Almoghrabi <Aalmoghrabi@tachyonhub.com>
2023-06-05 11:39:16 +0300

    TAC-5606 Invoices Paid before Due Date Percentage widget FE link to api

--- angular/src/app/shared/common/customizable-dashboard/widgets/host/paid-invoices-before-due-date/paid-invoices-before-due-date.component.ts
+++ angular/src/app/shared/common/customizable-dashboard/widgets/host/paid-invoices-before-due-date/paid-invoices-before-due-date.component.ts
@@ -1,5 +1,7 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
+import { GetInvoicesPaidBeforeDueDateOutput, TMSAndHostDashboardServiceProxy } from '@shared/service-proxies/service-proxies';
+import { finalize } from '@node_modules/rxjs/operators';
 
 @Component({
   selector: 'app-paid-invoices-before-due-date',
@@ -8,14 +10,29 @@ import { AppComponentBase } from '@shared/common/app-component-base';
 })
 export class PaidInvoicesBeforeDueDateComponent extends AppComponentBase implements OnInit {
   loading = false;
+  paidInvoiceBeforeDueDate: GetInvoicesPaidBeforeDueDateOutput;
 
-  constructor(private injector: Injector) {
+  constructor(private injector: Injector, private _TMSAndHostDashboardServiceProxy: TMSAndHostDashboardServiceProxy) {
     super(injector);
   }
 
-  ngOnInit(): void {
-    this.getData();
+  ngOnInit(): void {}
+
+  getData(event: { start: moment.Moment; end: moment.Moment }) {
+    this.loading = true;
+    this._TMSAndHostDashboardServiceProxy
+      .getInvoicesPaidBeforeDueDate()
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+        })
+      )
+      .subscribe((res) => {
+        this.paidInvoiceBeforeDueDate = res;
+      });
   }
 
-  getData() {}
+  selectedFilter(event: { start: moment.Moment; end: moment.Moment }) {
+    this.getData(event);
+  }
 }
