4cc8f3bed2c29bf068579b7b226c883bf4fca41c
Amjad Almoghrabi <Aalmoghrabi@tachyonhub.com>
2023-06-12 12:24:34 +0300

    TAC-4494 Price package filter with ability to export

--- angular/src/app/main/pricePackages/price-package/price-package.component.ts
+++ angular/src/app/main/pricePackages/price-package/price-package.component.ts
@@ -2,8 +2,11 @@ import { Component, Injector, OnInit } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import CustomStore from '@node_modules/devextreme/data/custom_store';
 import { LoadOptions } from '@node_modules/devextreme/data/load_options';
-import {PricePackageServiceProxy, ShippingRequestRouteType} from '@shared/service-proxies/service-proxies';
-import {EnumToArrayPipe} from '@shared/common/pipes/enum-to-array.pipe';
+import { PricePackageServiceProxy, ShippingRequestRouteType } from '@shared/service-proxies/service-proxies';
+import { EnumToArrayPipe } from '@shared/common/pipes/enum-to-array.pipe';
+import { Workbook } from '@node_modules/exceljs';
+import { exportDataGrid } from '@node_modules/devextreme/excel_exporter';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-price-package',
@@ -46,9 +49,38 @@ export class PricePackageComponent extends AppComponentBase implements OnInit {
     });
   }
 
-    getRouteTypeTitle(routeType): string {
-        let routeTypeTitle = this.routeTypes.find((item) => item.key === routeType?.toString())?.value;
+  getRouteTypeTitle(routeType): string {
+    let routeTypeTitle = this.routeTypes.find((item) => item.key === routeType?.toString())?.value;
 
-        return routeTypeTitle ?? '';
-    }
+    return routeTypeTitle ?? '';
+  }
+
+  onExporting(e) {
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet('PricePackages');
+
+    exportDataGrid({
+      component: e.component,
+      worksheet,
+      autoFilterEnabled: true,
+      customizeCell: ({ gridCell, excelCell }) => {
+        if (gridCell.rowType === 'data') {
+          if (gridCell.column.dataField === 'routeType') {
+            excelCell.value = this.getRouteTypeTitle(gridCell.value);
+          }
+          if (gridCell.column.dataField === 'status') {
+            excelCell.value = this.l(gridCell.value);
+          }
+        }
+        if (gridCell.rowType === 'group') {
+          excelCell.fill = { type: 'pattern', pattern: 'solid', fgColor: { argb: 'BEDFE6' } };
+        }
+      },
+    }).then(() => {
+      workbook.xlsx.writeBuffer().then((buffer) => {
+        saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'PricePackages.xlsx');
+      });
+    });
+    e.cancel = true;
+  }
 }
