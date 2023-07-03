65aa55e495b4befd326ff78f5b427b6723137df9
Mahmoud <M@tachyon.site>
2023-07-03 12:25:07 +0300

    Add 3.15.5 migration files to test branch

---
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20230703092427_3.15.5.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TACHYON.Migrations
+{
+    public partial class _3155 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PackingTypeId",
+                table: "ShippingRequestTrips",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSaas",
+                table: "RatingLogs",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShippingRequestTrips_PackingTypeId",
+                table: "ShippingRequestTrips",
+                column: "PackingTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShippingRequestTrips_PackingTypes_PackingTypeId",
+                table: "ShippingRequestTrips",
+                column: "PackingTypeId",
+                principalTable: "PackingTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShippingRequestTrips_PackingTypes_PackingTypeId",
+                table: "ShippingRequestTrips");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShippingRequestTrips_PackingTypeId",
+                table: "ShippingRequestTrips");
+
+            migrationBuilder.DropColumn(
+                name: "PackingTypeId",
+                table: "ShippingRequestTrips");
+
+            migrationBuilder.DropColumn(
+                name: "IsSaas",
+                table: "RatingLogs");
+        }
+    }
+}
