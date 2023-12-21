a0d517098dfb7f089eaf5d0f089055982d19f18f
Islam Al Khatib <Ialkhateeb@trustangle.com>
2023-12-21 12:40:52 +0200

    feat: Add migration for redeem codes and redemption codes
    This commit adds a new migration file `20231221102308_hotfix_redeemCode.cs` which creates two new tables `RedeemCodes` and `RedemptionCodes`.

--- aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20231221102308_hotfix_redeemCode.Designer.cs
+++ aspnet-core/src/TACHYON.EntityFrameworkCore/Migrations/20231221102308_hotfix_redeemCode.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using TACHYON.EntityFrameworkCore;
@@ -10,9 +11,10 @@ using TACHYON.EntityFrameworkCore;
 namespace TACHYON.Migrations
 {
     [DbContext(typeof(TACHYONDbContext))]
-    partial class TACHYONDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231221102308_hotfix_redeemCode")]
+    partial class hotfix_redeemCode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
